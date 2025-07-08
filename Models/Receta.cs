eccdc9221fe88bf5bc2ae43fc31ab56dcba9e09d
Galo Guevara <galo.guevara.torres@udla.edu.ec>
2025-07-08 07:15:22 -0500

    feat: crear modelo receta e importar sqlite

---
+++ Models/Receta.cs
@@ -0,0 +1,19 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuevaraExamenProg3.Models
+{
+    public class Receta
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Ingredientes { get; set; }
+        public int TiempoPreparacionMinutos { get; set; }
+        public bool EsVegetariana { get; set; }
+    }
+}
