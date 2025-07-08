f4fa3dee4918bfde474ba1ea12d2f96e973c50e8
Galo Guevara <galo.guevara.torres@udla.edu.ec>
2025-07-08 07:33:07 -0500

    feat: implementar RecetaService

--- Services/RecetaService.cs
+++ Services/RecetaService.cs
@@ -1,28 +1,65 @@
 ﻿using GuevaraExamenProg3.Interfaces;
 using GuevaraExamenProg3.Models;
+using SQLite;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using GuevaraExamenProg3.Models;
 
 namespace GuevaraExamenProg3.Services
 {
     public class RecetaService : IRecetaService
     {
-        public Task<List<string>> GetLogsAsync()
+        //conectar sqlite
+        private readonly string _dbPath = Path.Combine(FileSystem.AppDataDirectory, "recetas.db3");
+        private readonly string _logPath = Path.Combine(FileSystem.AppDataDirectory, "Logs_Guevara.txt");
+        private SQLiteAsyncConnection _connection;
+
+        //iniciar connection
+        private async Task InitAsync()
         {
-            throw new NotImplementedException();
+            if (_connection != null) { return; }
+
+            _connection = new SQLiteAsyncConnection(_dbPath);
+            await _connection.CreateTableAsync<Receta>();
         }
 
-        public Task<List<Receta>> GetRecetasAsync()
+        //obtener logs async
+        public async Task<List<string>> GetLogsAsync()
         {
-            throw new NotImplementedException();
+            if (File.Exists(_logPath))
+            {
+                return new List<string>(await File.ReadAllLinesAsync(_logPath));
+            }
+            return new List<string>();
+        }
+        
+        //obtener recetas async
+        public async Task<List<Receta>> GetRecetasAsync()
+        {
+            await InitAsync();
+
+            return await _connection.Table<Receta>().ToListAsync();
         }
 
-        public Task<bool> GuardarRecetaAsync(Receta receta)
+        //guardar recetas async
+        public async Task<bool> GuardarRecetaAsync(Receta receta)
         {
-            throw new NotImplementedException();
+            //regla
+            if (receta.TiempoPreparacionMinutos>180 && !receta.EsVegetariana)
+            {
+                return false;
+            }
+
+            await _connection.InsertAsync(receta);
+
+            string log = $"Se incluyo el registro {receta.Nombre} el {DateTime.Now:dd/MM/yyyy hh:mm}\n";
+
+            await File.AppendAllTextAsync(_logPath, log);
+
+            return true;
         }
     }
 }
