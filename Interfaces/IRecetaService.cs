2fa708ecc710d6a33b66446c64d93b1b70490e0d
Galo Guevara <galo.guevara.torres@udla.edu.ec>
2025-07-08 07:17:25 -0500

    feat: crear interfaz IRecetaService

---
+++ Interfaces/IRecetaService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GuevaraExamenProg3.Models;
+
+namespace GuevaraExamenProg3.Interfaces
+{
+    public interface IRecetaService
+    {
+        Task<List<Receta>> GetRecetasAsync();
+        Task<bool> GuardarRecetaAsync(Receta receta);
+        Task<List<string>> GetLogsAsync();
+    }
+}
