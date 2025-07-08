9d3ac52dcd41205c084aa37ad9a1bb590f9c39b4
Galo Guevara <galo.guevara.torres@udla.edu.ec>
2025-07-08 07:19:01 -0500

    feat: implementar interfaz en RecetaService

---
+++ Services/RecetaService.cs
@@ -0,0 +1,28 @@
+﻿using GuevaraExamenProg3.Interfaces;
+using GuevaraExamenProg3.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuevaraExamenProg3.Services
+{
+    public class RecetaService : IRecetaService
+    {
+        public Task<List<string>> GetLogsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Receta>> GetRecetasAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> GuardarRecetaAsync(Receta receta)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
